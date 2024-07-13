64145c841b9c3354cde93a23f6e2df37cfd9a05f
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-14 00:04:03 +1000

    feat: add test cases passed

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': 'jest-transform-stub',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',

d87423e01f4cfe86572df9138ce330e810e89dba
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-14 00:40:20 +1000

    feat: adjust the table

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,36 @@ import React from 'react';
 import './styles.scss';
 
 const App = () => {
+  const headers = ['Header 1', 'Header 2', 'Header 3'];
+  const data = [
+    { col1: 'Cell 1', col2: 'Cell 2', col3: 'Cell 3' },
+    { col1: 'Cell 4', col2: 'Cell 5', col3: 'Cell 6' },
+    { col1: 'Cell 7', col2: 'Cell 8', col3: 'Cell 9' },
+  ];
+
   return (
     <div className="container">
       <h1>Hello SCSS</h1>
       <button className="button">点击我</button>
       <div className="grid-table">
-        <div className="header">Header 1</div>
-        <div className="header">Header 2</div>
-        <div className="header">Header 3</div>
-        <div className="cell">Cell 1</div>
-        <div className="cell">Cell 2</div>
-        <div className="cell">Cell 3</div>
-        <div className="cell">Cell 4</div>
-        <div className="cell">Cell 5</div>
-        <div className="cell">Cell 6</div>
+        {headers.map((header, index) => (
+          <div key={index} className="header">
+            {header}
+          </div>
+        ))}
+        {data.map((row, rowIndex) => (
+          <>
+            <div key={`cell-${rowIndex}-1`} className="cell">
+              {row.col1}
+            </div>
+            <div key={`cell-${rowIndex}-2`} className="cell">
+              {row.col2}
+            </div>
+            <div key={`cell-${rowIndex}-3`} className="cell">
+              {row.col3}
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );

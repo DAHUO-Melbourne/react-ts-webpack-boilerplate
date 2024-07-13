af327d0426ae10ab0b14a2d9ce211cb7611e51ae
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-13 23:37:30 +1000

    fix: use react 18 as feature

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 /** @format */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// eslint-disable-next-line react/no-deprecated
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

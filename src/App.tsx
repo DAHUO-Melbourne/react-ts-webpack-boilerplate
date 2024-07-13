b8a5ce13f209dc18012f5e87d72c20975d2e5693
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-14 00:29:11 +1000

    feat: add grid table

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ const App = () => {
     <div className="container">
       <h1>Hello SCSS</h1>
       <button className="button">点击我</button>
+      <div className="grid-table">
+        <div className="header">Header 1</div>
+        <div className="header">Header 2</div>
+        <div className="header">Header 3</div>
+        <div className="cell">Cell 1</div>
+        <div className="cell">Cell 2</div>
+        <div className="cell">Cell 3</div>
+        <div className="cell">Cell 4</div>
+        <div className="cell">Cell 5</div>
+        <div className="cell">Cell 6</div>
+      </div>
     </div>
   );
 };
